a67f269f21f976ce0eeb005bb66df9bc6ddef540
Walid-Ouldali <ouldali02@hotmail.fr>
2022-10-11 04:50:42 +0200

    adding tp files

---
+++ app.js
@@ -0,0 +1,14 @@
+function grow(){
+	function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
+	var num = getRndInteger(80,100);
+		document.getElementById('title').innerHTML = num + '/100';
+		var num2 = document.getElementById('scuare');
+		num2.style.width = num + '%';
+		num2.style.borderRadius = '5px';	
+}
+
+document.getElementById('buton').addEventListener('click', grow);
+
+
